b339200af9ef45fa34f1825270f6b9f84bbb41f7
antonioasaro <antonio_asaro@hotmail.com>
2014-12-06 17:19:14 -0500

    .

--- core/src/com/antoniogdx/game/AntonioGdxGame.java
+++ core/src/com/antoniogdx/game/AntonioGdxGame.java
@@ -41,7 +41,7 @@ public class AntonioGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0.5f, 1.0f, 0, 1);
+		Gdx.gl.glClearColor(0.3f, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		camera.update();
