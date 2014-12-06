f09a2d60edbecbb179b589854dca767b34f4ba2a
antonioasaro <antonio_asaro@hotmail.com>
2014-12-06 09:46:16 -0500

    Added camera + dispose()

--- core/src/com/antoniogdx/game/AntonioGdxGame.java
+++ core/src/com/antoniogdx/game/AntonioGdxGame.java
@@ -4,18 +4,23 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.audio.Sound;
 
 public class AntonioGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture dropletImage;
-	Texture bucketImage;
-	Music rainMusic;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Texture dropletImage;
+	private Texture bucketImage;
+	private Music rainMusic;
 	
 	@Override
 	public void create () {
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		
 		batch = new SpriteBatch();
 		dropletImage = new Texture("droplet.png");
 		bucketImage = new Texture("bucket.png");
@@ -27,11 +32,35 @@ public class AntonioGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClearColor(0.5f, 1.0f, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		camera.update();
+		
 		batch.begin();
 		batch.draw(dropletImage, 200, 200);
 		batch.draw(bucketImage, 0, 0);
 		batch.end();
 	}
+	
+	   @Override
+	   public void dispose() {
+	      dropletImage.dispose();
+	      bucketImage.dispose();
+	      rainMusic.dispose();
+	      batch.dispose();
+	   }
+
+	   @Override
+	   public void resize(int width, int height) {
+	   }
+
+	   @Override
+	   public void pause() {
+	   }
+
+	   @Override
+	   public void resume() {
+	   }
+	
 }
--- desktop/src/com/antoniogdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/antoniogdx/game/desktop/DesktopLauncher.java
@@ -3,10 +3,15 @@ package com.antoniogdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.antoniogdx.game.AntonioGdxGame;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+	    config.title = "Droplets";
+	    config.width = 800;
+	    config.height = 480;
 		new LwjglApplication(new AntonioGdxGame(), config);
 	}
 }
+
