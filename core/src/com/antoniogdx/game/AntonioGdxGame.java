9036cb1af9292d6c35bbb7b91cccaddccf212387
antonioasaro <antonio_asaro@hotmail.com>
2014-12-06 08:47:33 -0500

    Add droplet png

--- core/src/com/antoniogdx/game/AntonioGdxGame.java
+++ core/src/com/antoniogdx/game/AntonioGdxGame.java
@@ -13,15 +13,15 @@ public class AntonioGdxGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("droplet.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 50, 100);
 		batch.end();
 	}
 }
