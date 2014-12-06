d5a068e17ae54032a85c48bdfeae03d8a63b9090
antonioasaro <antonio_asaro@hotmail.com>
2014-12-06 17:52:13 -0500

    Working

--- core/src/com/antoniogdx/game/AntonioGdxGame.java
+++ core/src/com/antoniogdx/game/AntonioGdxGame.java
@@ -1,15 +1,21 @@
 package com.antoniogdx.game;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
+import java.util.*;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class AntonioGdxGame extends ApplicationAdapter {
 	private OrthographicCamera camera;
@@ -17,7 +23,10 @@ public class AntonioGdxGame extends ApplicationAdapter {
 	private Texture dropletImage;
 	private Texture bucketImage;
 	private Music rainMusic;
+	private Sound dropSound;
 	private Rectangle bucket;
+	private Array<Rectangle> raindrops;
+	private long lastDropTime;
 	
 	@Override
 	public void create () {
@@ -27,18 +36,33 @@ public class AntonioGdxGame extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		dropletImage = new Texture("droplet.png");
 		bucketImage = new Texture("bucket.png");
+
+	    dropSound = Gdx.audio.newSound(Gdx.files.internal("drip.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+		rainMusic.setLooping(true);
+		rainMusic.play();
 		
         bucket = new Rectangle();
 	    bucket.x = 800 / 2 - 64 / 2;
 	    bucket.y = 20;
 	    bucket.width = 64;
 	    bucket.height = 64;
-		
-		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
-		rainMusic.setLooping(true);
-		rainMusic.play();
+	    
+	    raindrops = new Array<Rectangle>();
+	    spawnRaindrop();
 	}
 
+	private void spawnRaindrop() {
+	    Rectangle raindrop = new Rectangle();
+	    raindrop.x = MathUtils.random(0, 800-64);
+	    raindrop.y = 480;
+	    raindrop.width = 64;
+	    raindrop.height = 64;
+	    raindrops.add(raindrop);
+	    lastDropTime = TimeUtils.nanoTime();
+	}
+	
+	
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0.3f, 0, 0, 1);
@@ -49,7 +73,9 @@ public class AntonioGdxGame extends ApplicationAdapter {
 	    batch.setProjectionMatrix(camera.combined);		
 		batch.begin();
 		batch.draw(bucketImage, bucket.x, bucket.y);
-		batch.draw(dropletImage, 400, 400);
+	    for(Rectangle raindrop: raindrops) {
+	        batch.draw(dropletImage, raindrop.x, raindrop.y);
+	    }
 		batch.end();
 		
 	    if(Gdx.input.isTouched()) {
@@ -57,30 +83,41 @@ public class AntonioGdxGame extends ApplicationAdapter {
 	        touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 	        camera.unproject(touchPos);
 	        bucket.x = touchPos.x - 64 / 2;
-	     }
-	     if(bucket.x < 0) bucket.x = 0;
-	     if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+	    }
+	    if(bucket.x < 0) bucket.x = 0;
+	    if(bucket.x > 800 - 64) bucket.x = 800 - 64;
 
+        if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+ 	    java.util.Iterator<Rectangle> iter = raindrops.iterator();
+	    while(iter.hasNext()) {
+	        Rectangle raindrop = iter.next();
+	        raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+	        if(raindrop.y + 64 < 0) iter.remove();
+	        if(raindrop.overlaps(bucket)) {
+	           dropSound.play();
+	           iter.remove();
+	        }
+	    }
 	}
 	
-	   @Override
-	   public void dispose() {
-	      dropletImage.dispose();
+	@Override
+	public void dispose() {
+	    dropletImage.dispose();
 	      bucketImage.dispose();
 	      rainMusic.dispose();
 	      batch.dispose();
-	   }
+	}
 
-	   @Override
-	   public void resize(int width, int height) {
-	   }
+	@Override
+	public void resize(int width, int height) {
+	}
 
-	   @Override
-	   public void pause() {
-	   }
+	@Override
+	public void pause() {
+	}
 
-	   @Override
-	   public void resume() {
-	   }
+	@Override
+	public void resume() {
+	}
 	
 }
