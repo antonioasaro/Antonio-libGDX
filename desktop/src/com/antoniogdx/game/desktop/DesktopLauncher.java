31282697448ffab40f1d4d16cffa7671d43569c7
antonioasaro <antonio_asaro@hotmail.com>
2014-12-06 07:16:19 -0500

    Initial version

---
+++ desktop/src/com/antoniogdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.antoniogdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.antoniogdx.game.AntonioGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AntonioGdxGame(), config);
+	}
+}
