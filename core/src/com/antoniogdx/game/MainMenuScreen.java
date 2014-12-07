7ac6e9933ccb94abbe7fbd72c4b35d3becdb46e9
antonioasaro <antonio_asaro@hotmail.com>
2014-12-07 12:48:13 -0500

    Minor text location/colour changes

--- core/src/com/antoniogdx/game/MainMenuScreen.java
+++ core/src/com/antoniogdx/game/MainMenuScreen.java
@@ -17,15 +17,15 @@ public class MainMenuScreen implements Screen {
 	 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClearColor(0, 0.2f, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	 
 		camera.update();
 		game.batch.setProjectionMatrix(camera.combined);
 	 
 		game.batch.begin();
-		game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
-		game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+		game.font.draw(game.batch, "Welcome to Droplets!!! ", 300, 350);
+		game.font.draw(game.batch, "Tap anywhere to begin!", 300, 300);
 		game.batch.end();
 	 
 		if (Gdx.input.isTouched()) {
